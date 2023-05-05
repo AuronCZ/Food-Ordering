38fe1a3e051d22282623fc617a5ddb8d26492511
AuronCZ <ac51995@ubt-uni.net>
2023-05-05 18:22:24 +0200

    app commit

---
+++ frontend/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+    <div id="app">
+      <div v-if="admin">
+        <router-view></router-view>
+      </div>
+      <div v-else>
+        <AuthenticatedLayout />
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  import AuthenticatedLayout from './components/AuthenticatedLayout.vue';
+  import { mapActions, mapState } from 'vuex';
+  export default {
+    name: 'App',
+    components: {
+      AuthenticatedLayout
+    },
+    created() {
+      this.getFoodsData()
+    },
+    computed: {
+      ...mapState(["admin"])
+    },
+    methods: {
+      ...mapActions(["getFoodsData"])
+    }
+  }
+  </script>
+  
+  <style>
+  @import "./assets/css/global_style.css";
+  </style>
+  
---
+++ frontend/src/AuthenticatedLayout.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <NavBar />
+    <div class="auth-wrapper">
+      <div class="auth-inner">
+        <router-view></router-view>
+      </div>
+    </div>
+    <FooterComponent />
+  </div>
+</template>
+
+<script>
+import NavBar from './components/NavBar.vue';
+import FooterComponent from './components/FooterComponent.vue';
+export default {
+  name: 'AuthenticatedLayout',
+  components: {
+    NavBar,
+    FooterComponent
+  }
+}
+</script>
+
+<style>
+@import "./assets/css/global_style.css";
+</style>
